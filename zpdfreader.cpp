e09f20bb39da4fd8122fa165bb3a8cd938beacb5
kernel1024 <kernel1024@users.noreply.github.com>
2013-01-23 08:47:37 +0000

    Transition from poppler-qt4 to poppler-cpp dependency.

--- zpdfreader.cpp
+++ zpdfreader.cpp
@@ -1,11 +1,24 @@
 #include "zpdfreader.h"
 
+#ifdef WITH_POPPLER
+#include <poppler-page.h>
+
+using namespace poppler;
+#endif
+
 ZPdfReader::ZPdfReader(QObject *parent, QString filename) :
     ZAbstractReader(parent,filename)
 {
 #ifdef WITH_POPPLER
     doc = NULL;
+    renderer = new page_renderer();
 #endif
+}
+
+ZPdfReader::~ZPdfReader()
+{
+    closeFile();
+    delete renderer;
 }
 
 bool ZPdfReader::openFile()
@@ -21,12 +34,12 @@ bool ZPdfReader::openFile()
         return false;
     }
 
-    doc = Poppler::Document::load(fileName);
+    doc = document::load_from_file(fileName.toStdString());
 
     if (doc==NULL)
         return false;
 
-    for (int i=0;i<doc->numPages();i++) {
+    for (int i=0;i<doc->pages();i++) {
         sortList << ZFileEntry(QString("%1").arg(i,6,10,QChar('0')),i);
     }
 
@@ -64,15 +77,24 @@ QByteArray ZPdfReader::loadPage(int num)
     if (num<0 || num>=sortList.count()) return res;
 
     int idx = sortList.at(num).idx;
-    Poppler::Page* page = doc->page(idx);
-    if (page!=NULL) {
-        double dpi = 72.0*((double)zg->pdfRenderWidth)/page->pageSizeF().width();
-        QImage img = page->renderToImage(dpi,dpi);
-        delete page;
+    page* p = doc->create_page(idx);
+    if (p!=NULL) {
+        double dpi = 72.0*((double)zg->pdfRenderWidth)/p->page_rect().width();
+        image img = renderer->render_page(p,dpi,dpi);
+        QImage qimg;
+        if (img.is_valid()) {
+            if (img.format()==image::format_mono)
+                qimg = QImage((uchar*)img.data(),img.width(),img.height(),QImage::Format_Mono);
+            else if (img.format()==image::format_rgb24)
+                qimg = QImage((uchar*)img.data(),img.width(),img.height(),QImage::Format_RGB888);
+            else if (img.format()==image::format_argb32)
+                qimg = QImage((uchar*)img.data(),img.width(),img.height(),QImage::Format_ARGB32);
+        }
+        delete p;
         QBuffer buf(&res);
         buf.open(QIODevice::WriteOnly);
-        img.save(&buf,"BMP");
-        img = QImage();
+        qimg.save(&buf,"BMP");
+        qimg = QImage();
     }
 #endif
     return res;
--- zpdfreader.h
+++ zpdfreader.h
@@ -2,9 +2,12 @@
 #define ZPDFREADER_H
 
 #include <QtCore>
+
 #ifdef WITH_POPPLER
-#include <poppler-qt4.h>
+#include <poppler-document.h>
+#include <poppler-page-renderer.h>
 #endif
+
 #include "zabstractreader.h"
 
 class ZPdfReader : public ZAbstractReader
@@ -12,11 +15,13 @@ class ZPdfReader : public ZAbstractReader
     Q_OBJECT
 protected:
 #ifdef WITH_POPPLER
-    Poppler::Document* doc;
+    poppler::document* doc;
+    poppler::page_renderer* renderer;
 #endif
 
 public:
     explicit ZPdfReader(QObject *parent, QString filename);
+    ~ZPdfReader();
     bool openFile();
     void closeFile();
     QByteArray loadPage(int num);
